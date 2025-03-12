3556b7ea28e697cb16fe753641e41ef072a913b8
Ayumu-Mitoma <v24e3028@oita-u.ac.jp>
2025-03-12 09:57:19 +0000

    a

---
+++ Wisteria_App_Mitoma/harmonic_tone_visible/const.py
@@ -0,0 +1,11 @@
+
+BINS_PER_OCTAVE = 36
+NUM_OCTAVE = 7
+SR = 44100
+HOP_LENGTH = 1024
+
+tone = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+LOW_TUNING = -0.7
+DEFALT_TUNING = 1.0
+HIGH_TUNING = +1.7
