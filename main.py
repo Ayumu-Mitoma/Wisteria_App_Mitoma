b2cec904142acfeae0d91af6f942c63c3ed6c8e9
Ayumu-Mitoma <v24e3028@oita-u.ac.jp>
2025-03-12 15:28:47 +0000

    aa

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-with st.sidebar:
-    st.page_link("main.py", label="ホーム", icon="🏠")
-    st.page_link("pages/harmonic_tone_visible.py", label="倍音見える君", icon="🎶")
-    st.page_link("pages/pdf_reader.py", label="楽譜読める君", icon="🎶")
+st.set_page_config(page_title="ウィステリア用アプリ created by 三苫", page_icon="🎼")
+
+top_page = st.Page(page="contents/home.py", title="ホーム", icon="🏠")
+harmonic = st.Page(page="contents/harmonic_tone_visible.py", title="倍音見える君", page_icon="🎶")
+pdf_reader = st.Page(page="contents/pdf_reader.py", title="楽譜読み込み君", page_icon="🎶")
+pg = st.navigation([top_page, harmonic, pdf_reader])
+pg.run()
