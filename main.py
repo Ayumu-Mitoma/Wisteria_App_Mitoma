47229f99b51c9434a769e379053b614142887e4a
Ayumu-Mitoma <v24e3028@oita-u.ac.jp>
2025-03-12 15:37:57 +0000

    aa

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ st.set_page_config(page_title="ウィステリア用アプリ created by 三苫"
 
 top_page = st.Page(page="contents/home.py", title="ホーム", icon="🏠")
 harmonic = st.Page(page="contents/harmonic_tone_visible.py", title="倍音見える君", icon="🎶")
-pdf_reader = st.Page(page="contents/pdf_reader.py", title="楽譜読み込み君", icon="🎶")
-pg = st.navigation([top_page, harmonic, pdf_reader])
+#pdf_reader = st.Page(page="contents/pdf_reader.py", title="楽譜見える君", icon="🎶")
+pg = st.navigation([top_page, harmonic])
 pg.run()
