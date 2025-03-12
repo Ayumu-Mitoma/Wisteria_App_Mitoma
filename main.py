aea2d6077efc72301d3bdef10083005d4b6b28b5
Ayumu-Mitoma <v24e3028@oita-u.ac.jp>
2025-03-12 13:08:35 +0000

    aa

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 with st.sidebar:
     st.page_link("main.py", label="ホーム", icon="🏠")
-    st.page_link("harmonic_tone_visible.py", label="倍音見える君", icon="1️⃣")
-    st.page_link("pdf_reader.py", label="楽譜読める君", icon="2️⃣")
+    st.page_link("harmonic_tone_visible.py", label="倍音見える君", icon="1")
+    st.page_link("pdf_reader.py", label="楽譜読める君", icon="2")
