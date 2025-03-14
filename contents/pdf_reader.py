94bab8719e78dec22cb7e2b7f1d3aebc46631c26
Ayumu-Mitoma <v24e3028@oita-u.ac.jp>
2025-03-14 20:35:33 +0000

    ll

--- contents/pdf_reader.py
+++ contents/pdf_reader.py
@@ -5,13 +5,12 @@ st.header("楽譜見える君")
 st.text("自分のスマホにある楽譜のpdfファイルを選択してね")
 
 st.session_state["visible"] = False
-score_file = st.file_uploader("楽譜のpdfファイルを選択")
+score_file = st.file_uploader("楽譜のpdfファイルを選択", type=["pdf"])
 
 if score_file is not None:
     file_name = score_file.name.lower()
     if file_name.endswith(".pdf"):
-        st.text(score_file)
-        images = pp.pdf_to_images(score_file)
+        images = pp.pdf_to_images(score_file.read)
         st.text(len(images))
         if len(images) != 0:
             st.session_state["visible"] = True
