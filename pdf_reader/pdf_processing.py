b33fd3a1c7803d331b884e4b008b429717fe0c30
Ayumu-Mitoma <v24e3028@oita-u.ac.jp>
2025-03-14 20:34:29 +0000

    cc

--- pdf_reader/pdf_processing.py
+++ pdf_reader/pdf_processing.py
@@ -1,9 +1,9 @@
 import numpy as np
 import glob
 from pdf_reader import const as C
-from pdf2image import convert_from_path
+from pdf2image import convert_from_bytes
 
 def pdf_to_images(file):
-    images = convert_from_path(file, dpi=600)
+    images = convert_from_bytes(file, dpi=600)
     return images
 
