290520c1492fc625f9e73be19762dff92d08eb92
Ayumu-Mitoma <v24e3028@oita-u.ac.jp>
2025-03-15 04:38:19 +0000

    ss

--- pdf_reader/pdf_processing.py
+++ pdf_reader/pdf_processing.py
@@ -2,9 +2,16 @@ import numpy as np
 import glob
 from pdf_reader import const as C
 import fitz
+from PIL import Image
 
 def pdf_to_images(file):
     docs = fitz.open(stream=file, filetype="pdf")
-    images = [page.get_pixmap() for page in docs]
-    return [img.pil_save() for img in images]
+    images = []
+
+    for page in docs:
+        pix = page.get_pixmap()
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        images.append(img)
+
+    return images
 
