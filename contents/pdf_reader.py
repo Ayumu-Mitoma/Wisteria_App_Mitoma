0347ebe2009bcc87158af5534dc65b455ffebb6b
Ayumu-Mitoma <v24e3028@oita-u.ac.jp>
2025-03-15 05:40:47 +0000

    rr

--- contents/pdf_reader.py
+++ contents/pdf_reader.py
@@ -11,7 +11,6 @@ if score_file is not None:
     file_name = score_file.name.lower()
     if file_name.endswith(".pdf"):
         images = pp.pdf_to_images(score_file.read())
-        st.text(len(images))
         if len(images) != 0:
             st.session_state["visible"] = True
     else:
@@ -19,4 +18,21 @@ if score_file is not None:
         st.stop()
 
 if st.session_state["visible"] == True:
-    pass
+    if "current_page" not in st.session_state:
+        st.session_state.current_page = 0
+
+    total_pages = len(images)
+    current_page = st.session_state.current_page
+
+    img_io = images[current_page]
+    st.image(img_io, caption=f"{current_page+1}/{total_pages} ページ")
+
+    cols = st.columns([1,1])
+    with cols[0]:
+        if st.button("前のページ", key="prev") and current_page > 0:
+            st.session_state.current_page -= 1
+            st.rerun()
+    with cols[1]:
+        if st.button("次のページ", key="next") and current_page < total_pages -1:
+            st.session_state.current_page += 1
+            st.rerun()
